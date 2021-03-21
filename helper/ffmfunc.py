59a23c7431151a8a381d1431414e99afca3a1ead
Yausika <73742151+huseynzade2003@users.noreply.github.com>
2021-03-22 00:53:36 +0400

    Update ffmfunc.py

--- helper/ffmfunc.py
+++ helper/ffmfunc.py
@@ -4,11 +4,11 @@ import json
 
 def probe(vid_file_path):
     """
-    ffprobe əmr sətrindən bir json verin
-    @vid_file_path : Video faylının mütləq (tam) yolu, sətri.
+    Give a json from ffprobe command line
+    @vid_file_path : The absolute (full) path of the video file, string.
     """
     if type(vid_file_path) != str:
-        raise Exception('Ffprobe'ya faylın tam bir fayl yolu verin')
+        raise Exception('Give ffprobe a full file path of the file')
 
     command = ["ffprobe",
                "-loglevel", "quiet",
@@ -43,4 +43,4 @@ def duration(vid_file_path):
 
 
 if __name__ == "__main__":
-    print(duration("examplefile.mp4"))  # 10.008
+    print(duration("examplefile.mp4"))
